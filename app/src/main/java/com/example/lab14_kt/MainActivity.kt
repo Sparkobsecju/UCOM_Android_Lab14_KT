72d2d780bff260dcb878eb173ae126c92a74b2c5
Sivan Chen <ryan1825114@gmail.com>
2024-06-27 11:11:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lab14_kt/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.lab14_kt
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private var counter1: Int = 0
+    private var counter2: Int = 0
+    private companion object {
+        private lateinit var ;
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+
+}
